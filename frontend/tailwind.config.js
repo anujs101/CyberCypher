974ecf847bde517b4e4660d867bf02323aaebce3
unknown <acharyadevesh1105@gmail.com>
2025-02-16 08:53:46 +0530

    some commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          'spin-slow': 'spin 4s linear infinite', // Customize the speed here
+        },
+      },
+    },
+  }
+  
